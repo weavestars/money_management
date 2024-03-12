ff8874f9cb7607c53cb71b1b510baf632c828a35
weavestars <g.aelinex@gmail.com>
2024-03-12 21:46:24 +0900

    recoded

--- main.py
+++ main.py
@@ -3,33 +3,55 @@ Made to practice python.
 Will be used to manage my budget.
 """
 from method import budget
+from os import system
 
 myBudget=budget()
-
+system("cls")
 
 while True:
+    
     sel=input(
 """
 무엇을 하시겠습니까?
 1. 현재 보유 자산 수정
 2. 고정 수입 수정/추가
 3. 고정 지출 수정/추가
-4. 현재 입력 정보 확인
-5. 나가기
+4. 변동 수입 수정/추가
+5. 변동 지출 수정/추가
+6. 현재 정보 출력
+7. 저장
+8. 불러오기 (입력하신 정보가 덮어씌워집니다!))
+9. 화면 지우기
+10. 나가기
 """)
     if sel=='1':
-        myBudget.setNewBudget()
+        myBudget.editCurrentBudget()
 
     elif sel=='2':
-        myBudget.setIncome()
+        myBudget.inputData(myBudget.inReasonBig, myBudget.f_in_, "수입")
 
     elif sel=='3':
-        myBudget.setOutcome()
+        myBudget.inputData(myBudget.outReasonBig, myBudget.f_out_, "지출")
 
     elif sel=='4':
-        print(myBudget)
+        myBudget.inputData(myBudget.inReasonBig, myBudget.in_, "수입")
 
     elif sel=='5':
-        print("종료되었습니다.")
+        myBudget.inputData(myBudget.outReasonBig, myBudget.out_, "지출")
+    
+    elif sel=='6':
+        print(myBudget)
 
+    elif sel=='7':
+        pass
+
+    elif sel=='8':
+        pass
+
+    elif sel=='9':
+        system("cls")
+
+    elif sel=='10':
+        print("종료되었습니다.")
+        break
 
--- method.py
+++ method.py
@@ -1,168 +1,80 @@
-import datetime
-
-today=datetime.date.today()
-class setBudgetReason():
-    """
-    수입/지출 대분류 리스트
-    """
+class budget():
     def __init__(self):
-        self.IncomeReasonList=['용돈','아르바이트','투자','장학금', '기타']
-        self.OutcomeReasonList=['여가', '식비', '교통비', '투자', '기타']
+        self.current_budget=0
+        self.f_in_=[]
+        self.f_out_=[]
+        self.in_=[]
+        self.out_=[]
+        self.inReasonBig=["용돈","투자","장학금","기타"]
+        self.outReasonBig=["여가","투자","식비","기타"]
 
-    """
-    수입 대분류 지정 
-    리턴값: str
-    """
-    def setIncomeReasonBig(self):
-        print(
-"""
-=======대분류=======
-1. 용돈
-2. 아르바이트
-3. 투자
-4. 장학금
-5. 기타
-====================
+    def editCurrentBudget(self):
+        self.current_budget=input("현재 자산을 입력하세요.\n")
 
-""")
-        a=input("해당하는 번호를 입력하세요.")
-        return self.IncomeReasonList[a]
-        
-    """
-    지출 대분류 지정
-    리턴값: str
-    """
-    def setOutcomeReasonBig(self):
-        print(
-"""
-=======대분류=======
-1. 용돈
-2. 아르바이트
-3. 투자
-4. 장학금
-====================
+    def inputData(self, destination1, destination2, string): 
+        """destintion1: 대분류리스트, destination2: f_in 등"""
+        temporaryList=[]
+        count=1
 
-""")
-        a=input("해당하는 번호를 입력하세요.")
-        return self.IncomeReasonList[a]
-        
+        print(f"===========")
+        for v in destination1:
+            temp=f"{str(count)}. "+v
+            print(temp)
+            count+=1
+        print(f"==========")
 
-class budget(setBudgetReason):
-    """자산 정보 목록"""
-    def __init__(self):
-        self.budget=input("현재 총 자산을 입력하세요.\n")
-        self.f_income=[]
-        self.f_outcome=[]
+        sel=input("대분류를 선택하세요. (숫자 입력)\n")
+        temporaryList.append(destination1[int(sel)])
 
-    """현재 자산 새로 설정(계산값과 실제 값이 다를 때 사용)"""
-    def setNewBudget(self):
-        self.budget=input("현재 총 자산을 새로 입력하세요.\n")
+        sel=input("소분류를 입력하세요.\n")
+        temporaryList.append(sel)
 
-    def setIncome(self):
-        sel=input(
-"""
-======고정 수입 설정======
-1. 신규
-2. 수정
-3. 제거
-=========================
-""")
-        if sel=='1':
-            temp=[]
-            temp.append(setBudgetReason.setIncomeReasonBig())
-            temp.append(input("소분류를 입력하세요:"))
-            temp.append(input("금액을 입력하세요:"))
-            self.f_income.append(temp)
+        sel=input("금액을 입력하세요\n")
+        temporaryList.append(str(sel))
 
-        elif sel=='2':
-            count=1
-            for value in self.f_income:
-                print(f"{count}"+str(value))
-                count+=1
+        sel=input(f"{string} 발생 월일을 입력하세요. (02월 17일: 0217)")
+        temporaryList.append(sel)
 
-            choice=input("어떤 항목을 수정하시겠습니까?")
-            self.f_income[choice-1][0]=setBudgetReason.setIncomeReasonBig()
-            self.f_income[choice-1][1]=input("소분류를 입력하세요:")
-            self.f_income[choice-1][2]=input("금액을 입력하세요:")
+        while True:
+            if temporaryList in destination2:
+                choice=str(input("이미 동일하게 입력하신 항목이 있습니다. 계속하시겠습니까? (Y/N)\n")).upper()
+                if choice=='Y':
+                    destination2.append(temporaryList)
+                    print("성공적으로 입력되었습니다.")
+                    break
 
-        elif sel=='3':
-            count=1
-            for value in self.f_income:
-                print(f"{count}"+str(value))
-                count+=1
+                elif choice =='N':
+                    print("성공적으로 취소되었습니다.")
+                    break
 
-            choice=input("어떤 항목을 삭제하시겠습니까?")
-
-            del self.f_income[choice]
-            for value in self.f_income:
-                print(value)
-            print("성공적으로 삭제되었습니다!")
-
-
-    def setOutcome(self):
-        sel=input(
-"""
-======고정 지출 설정======
-1. 신규
-2. 수정
-3. 제거
-=========================
-""")
-        if sel==1:
-            temp=[]
-            temp.append(setBudgetReason.setOutcomeReasonBig())
-            temp.append(input("소분류를 입력하세요:"))
-            temp.append(input("금액을 입력하세요:"))
-            self.f_income.append(temp)
-
-        elif sel==2:
-            count=1
-            for value in self.f_outcome:
-                print(f"{count}"+str(value))
-                count+=1
-
-            choice=input("어떤 항목을 수정하시겠습니까?")
-            self.f_outcome[choice-1][0]=setBudgetReason.setOutcomeReasonBig()
-            self.f_outcome[choice-1][1]=input("소분류를 입력하세요:")
-            self.f_outcome[choice-1][2]=input("금액을 입력하세요:")
-
-        elif sel==3:
-            count=1
-            for value in self.f_outcome:
-                print(f"{count}"+str(value))
-                count+=1
-
-            choice=input("어떤 항목을 삭제하시겠습니까?")
-
-            del self.f_outcome[choice]
-            for value in self.f_outcome:
-                print(value)
-            print("성공적으로 삭제되었습니다!")
+                else:
+                    print("정확히 입력하십시오. (Y/N 중 하나 입력)")
+            else:
+                destination2.append(temporaryList)
+                print("성공적으로 입력되었습니다.")
+                break
 
     def __str__(self):
-        print(f"현재 총 자산은 {self.budget} 입니다.")
-        print("고정 수입은 다음과 같습니다.")
-
-        if(len(self.f_income)>0):
-            for value in self.f_outcome:
-                print(value)
+        total_f_in=self.getTotalValue(self.f_in_)
+        total_f_out=self.getTotalValue(self.f_out_)
+        total_out=self.getTotalValue(self.out_)
+        total_in=self.getTotalValue(self.in_)
+        
+        return f"""
+현재 자산:{self.current_budget}
+고정수입합계:{total_f_in}
+고정지출합계:{total_f_out}
+"""
+    def getTotalValue(self,destination):
+        temp=0
+        if len(destination)==0:
+            return 0
         else:
-            print("없음")
+            for v in destination:
+                temp+=int(v[2])
+            return temp
 
-        print("고정 지출은 다음과 같습니다.")
-
-        if(len(self.f_outcome)>0):
-            for value in self.f_outcome:
-                print(value)
-        else:
-            print("없음")
-
-            
-
-class budgetSet(budget):
-    def __init__(self):
-        super()
-
-    
-
- 
+"""이번 달 수입(변동):{total_in}
+이번 달 지출(변동):{total_out}
+이번 달 손익:{total_f_in+total_in-total_f_out-total_out}
+"""
